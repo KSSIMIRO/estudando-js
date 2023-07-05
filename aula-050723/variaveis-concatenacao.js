71f6a4a263d1f8701dda718c7fd93cf2d77c8f86
KSSIMIRO <matheus.farcas@gmail.com>
2023-07-05 09:50:49 -0300

    concatenação

---
+++ aula-050723/variaveis-concatenacao.js
@@ -0,0 +1,21 @@
+//Escopo Global (var X let)
+
+let numero1 = 22;
+// console.log(numero1);
+
+console.log(numero2)
+// var numero2 = 24
+
+// Operador de concatenação +
+const numero = 2 + 3; // Operador ede adição
+const texto = "O número é " + numero; // Operador de concatenação
+// console.log(texto)
+
+console.log("A soma de " + 2 + " com " + 3 + " é igual a " + numero);
+
+let idade = 18;
+let mensagem = (idade >= 18) ? "Sua idade é " + idade + " pode ser preso!" : "Sua idade é " + idade + " vai chegar sua vez! te cuida"
+// console.log(mensagem);
+
+const nome = "Matheus";
+console.log("Bem-Vindo, " + nome);
