280dd63194ea9add3cef52fadcb2e8afe025f367
KSSIMIRO <matheus.farcas@gmail.com>
2023-07-07 11:04:47 -0300

    atividade 07/07/23

---
+++ aula-070723/atividades.js
@@ -0,0 +1,62 @@
+// Escreva um programa que imprima os números de 1 a 20 usando um loop for.
+for(let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+console.log("==================")
+
+// Escreva um programa que exiba os números de Fibonacci até o décimo termo. Os números de Fibonacci são formados pela sequência 0, 1, 1, 2, 3, 5, 8, 13, ... em que cada número é a soma dos dois anteriores. Use um loop while para gerar a sequência.
+let a = 0;
+let b = 1;
+let count = 0;
+
+while (count < 10) {
+    console.log(a);
+    let temp = a;
+    a = b;
+    b = temp + b;
+    count++;
+}
+
+
+console.log("===================")
+
+// Crie um programa que receba um número e calcule a soma de todos os números ímpares até esse número. Use um loop for e uma estrutura condicional if para verificar se cada número é ímpar.
+
+let numero = 100
+for (let i = 1; i < numero; i++) {
+    if (i % 2 == 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+console.log("==================")
+
+// Faça um programa que receba um número e exiba a tabuada da soma desse número usando um loop for.
+
+let numero2 = 12
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero2 + i;
+    console.log(numero2, "+", i, "=", resultado);
+}
+
+console.log("==================")
+
+//Crie um programa que exiba todos os números múltiplos de 7 de 1 a 100 usando um loop for e uma estrutura condicional if para verificar se cada número é múltiplo de 7.
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 7 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("==================")
+//Escreva um programa que receba um número e desenhe na tela um triângulo formado por "*".
+linha = "* ";
+let lado = 0;
+while(lado < 7) {
+    console.log(linha);
+    linha += "* ";
+    lado++
+}
--- aula-070723/repeticoes.js
+++ aula-070723/repeticoes.js
@@ -11,25 +11,25 @@ while(condição) {
 */
 
 // detenhe um quadrado de # na tela
-let lado = 7;
-for(let i = 0; i < 7; i++) {
-    let linha = "";
-    for(let j = 0; j < lado; j++) {
-        linha += "# ";
-    }
-    console.log(linha);
-}
+// let lado = 7;
+// for(let i = 0; i < 7; i++) {
+//     let linha = "";
+//     for(let j = 0; j < lado; j++) {
+//         linha += "# ";
+//     }
+//     console.log(linha);
+// }
 
-console.log("=========================================")
+// console.log("=========================================")
 
-// Triangulo
-linha2 = "# ";
-let lado2 = 0;
-while(lado2 < 7) {
-    console.log(linha2);
-    linha2 += "# ";
-    lado2++
-}
+// // Triangulo
+// linha2 = "# ";
+// let lado2 = 0;
+// while(lado2 < 7) {
+//     console.log(linha2);
+//     linha2 += "# ";
+//     lado2++
+// }
 
 
 // break e continue
