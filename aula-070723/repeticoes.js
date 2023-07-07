68233873b87578029b16e1d520bd3f2955ea73f6
KSSIMIRO <matheus.farcas@gmail.com>
2023-07-07 10:19:51 -0300

    brak e continuos

---
+++ aula-070723/repeticoes.js
@@ -0,0 +1,49 @@
+/*
+// Uma quantidade determinada de repetições
+for(inicialização; condição; incrementação) {
+    //repetição ...
+}
+
+// Uma quantidade indeterminada de repetições
+while(condição) {
+    // repetição
+}
+*/
+
+// detenhe um quadrado de # na tela
+let lado = 7;
+for(let i = 0; i < 7; i++) {
+    let linha = "";
+    for(let j = 0; j < lado; j++) {
+        linha += "# ";
+    }
+    console.log(linha);
+}
+
+console.log("=========================================")
+
+// Triangulo
+linha2 = "# ";
+let lado2 = 0;
+while(lado2 < 7) {
+    console.log(linha2);
+    linha2 += "# ";
+    lado2++
+}
+
+
+// break e continue
+// ignora todos os impares
+// for(let i = 1; i <= 20; i++) {
+//     if(!(i % 2 == 0)) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 100; i++) {
+//     if((i % 8 == 0) && (i != 8)) {
+//         console.log(i);
+//         break;
+//     }
+// }
